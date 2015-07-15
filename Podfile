ec277affbb444eaaab8060d7aad280bf9856394a
chase.stone <chase.stone@daumkakao.com>
2015-07-15 19:04:26 +0900

    add JSONKit & request stock list

--- Podfile
+++ Podfile
@@ -2,4 +2,5 @@ target 'StockToday' do
 	pod 'AFNetworking'
 	pod 'CocoaAsyncSocket'
 	pod 'FMDB'
+	pod 'JSONKit'
 end
--- StockToday/STMainViewController.m
+++ StockToday/STMainViewController.m
@@ -11,11 +11,14 @@
 #import <GCDAsyncSocket.h>
 #import <AFNetworking.h>
 #import <FMDB.h>
+#import <JSONKit.h>
 
 @interface STMainViewController ()
 
 @property (weak) IBOutlet NSButton* updateStockList;
 @property (weak) IBOutlet NSButton* updateStockData;
+
+@property (strong) AFHTTPRequestOperationManager *operationManager;
 
 @end
 
@@ -24,10 +27,36 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.operationManager = [[AFHTTPRequestOperationManager alloc] init];
+
 }
 
 - (IBAction)updateStockListPressed:(id)sender
 {
+    // http://stock.daum.net/xml/xmlallpanel.daum?stype=P&type=S //KOSPI (가나다=S / 업종순=U)
+    // http://stock.daum.net/xml/xmlallpanel.daum?stype=Q&type=S //KOSDAQ (가나다=S / 업종순=U)
+
+    self.operationManager.responseSerializer = [AFHTTPResponseSerializer serializer];
+
+//    AFHTTPRequestSerializer *requestSerializer = self.operationManager.requestSerializer;
+//    [requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+//    self.operationManager.responseSerializer = [AFJSONResponseSerializer serializer];
+
+    NSString *url = @"http://stock.daum.net/xml/xmlallpanel.daum?stype=P&type=S";
+    
+    [self.operationManager GET:url parameters:nil
+                  success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                      NSString *data = [[NSString alloc] initWithUTF8String:[(NSData *)responseObject bytes]];
+
+                      NSDictionary* info = [data objectFromJSONString];
+                      
+                      
+                      NSLog(@"SUCCESS - %@", data);
+                  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                      NSLog(@"FAIL - %@", error);
+                  }];
+
     
 }
 
