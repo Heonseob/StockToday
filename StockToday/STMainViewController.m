b325fc5834971155000746920a727e5c11b2be9e
chase.stone <chase.stone@daumkakao.com>
2015-07-16 18:06:06 +0900

    create DB schema

--- StockToday/STMainViewController.m
+++ StockToday/STMainViewController.m
@@ -17,6 +17,10 @@
 
 @property (weak) IBOutlet NSButton* updateStockList;
 @property (weak) IBOutlet NSButton* updateStockData;
+@property (weak) IBOutlet NSButton* openDatabase;
+
+@property (strong) FMDatabase *db;
+@property (strong) NSOperationQueue *queue;
 
 @property (strong) AFHTTPRequestOperationManager *operationManager;
 
@@ -34,6 +38,10 @@
 //    AFHTTPRequestSerializer *requestSerializer = self.operationManager.requestSerializer;
 //    [requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
 //    self.operationManager.responseSerializer = [AFJSONResponseSerializer serializer];
+
+    self.queue = [NSOperationQueue new];
+    self.queue.maxConcurrentOperationCount = 1;
+    self.queue.name = @"StockToday";
 }
 
 - (IBAction)updateStockListPressed:(id)sender
@@ -45,6 +53,75 @@
 {
     [self updateStockItemList:NO];
 }
+
+- (IBAction)openDatabase:(id)sender
+{
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *dir = [paths objectAtIndex:0];
+    dir = [dir stringByAppendingPathComponent:@"StockToday.db"];
+    
+    BOOL openSuccess = NO;
+    
+    do {
+        self.db = [FMDatabase databaseWithPath:dir];
+        if (self.db == nil || ![self.db open])
+        {
+            NSLog(@"Couldn't open database at path: %@", dir);
+            break;
+        }
+        
+        if ([self.db getSchema] == nil)
+        {
+            NSLog(@"database is invalid: %@", self.db.lastError);
+            NSError *error;
+            if ([[NSFileManager defaultManager] removeItemAtPath:dir error:&error])
+                continue;
+
+            NSLog(@"Couldn't delete database file: %@", error);
+            break;
+        }
+        
+        openSuccess = YES;
+        
+    } while (NO);
+    
+    [self.db setShouldCacheStatements:YES];
+    [self.db setCrashOnErrors:YES];
+    [self.db setLogsErrors:YES];
+
+    @try
+    {
+//        [self.db executeUpdate:@"PRAGMA secure_delete = 1"];
+//        [self.db executeUpdate:@"PRAGMA journal_mode = wal"];
+//        [self.db executeUpdate:@"PRAGMA synchronous = 1"];
+//        [self.db executeUpdate:@"PRAGMA fullfsync = 1"];
+        
+        [self.db beginTransaction];
+        
+        for (NSString *query in [self queryArrayForCreatingSchema])
+        {
+            if ([self.db executeUpdate:query] == NO)
+            {
+                [[NSException exceptionWithName:@"DB_CREATE"
+                                         reason:self.db.lastError.localizedDescription
+                                       userInfo:@{ @"error": self.db.lastError }] raise];
+            }
+        }
+        
+        [self.db commit];
+    }
+    @catch (NSException *exception)
+    {
+        NSError *error;
+        if (![[NSFileManager defaultManager] removeItemAtPath:self.db.databasePath error:&error])
+            NSLog(@"Couldn't delete database file: %@", error);
+        @throw;
+    }
+
+    [self.db close];
+    
+}
+
 
 - (void) updateStockItemList:(BOOL)kospi
 {
@@ -108,6 +185,50 @@
                        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
                            NSLog(@"StockItemList FAIL - %@", error);
                        }];
+}
+
+- (NSArray *)queryArrayForCreatingSchema
+{
+    return @[
+             (@"CREATE TABLE SHMarket ("
+               "    marketType	INTEGER NOT NULL UNIQUE,"
+               "    name        TEXT NOT NULL UNIQUE,"
+               "    up_max      REAL NOT NULL DEFAULT 0.30,"
+               "    down_max	REAL NOT NULL DEFAULT -0.30,"
+               "    PRIMARY KEY(marketType)"
+               ");"),
+             
+             (@"CREATE TABLE SHItemInfo ("
+               "    code        TEXT NOT NULL UNIQUE,"
+               "    name        TEXT NOT NULL UNIQUE,"
+               "    marketType	INTEGER NOT NULL,"
+               "    branchType	INTEGER DEFAULT 0,"
+               "    tax_buy     REAL NOT NULL DEFAULT 0.0,"
+               "    tax_sell	REAL NOT NULL DEFAULT 0.0030,"
+               "    fee_buy     REAL NOT NULL DEFAULT 0.000150,"
+               "    fee_sell	REAL NOT NULL DEFAULT 0.000150,"
+               "    PRIMARY KEY(code,name)"
+               ");"),
+             
+             (@"CREATE INDEX idx_itemInfo_code ON SHItemInfo (code);"),
+             (@"CREATE INDEX idx_itemInfo_name ON SHItemInfo (name);"),
+             
+             (@"CREATE TABLE SHItem ("
+               "    code        TEXT NOT NULL,"
+               "    date        INTEGER NOT NULL,"
+               "    start       INTEGER NOT NULL,"
+               "    high        INTEGER NOT NULL,"
+               "    low         INTEGER NOT NULL,"
+               "    end         INTEGER NOT NULL,"
+               "    updown      INTEGER NOT NULL,"
+               "    rate        REAL NOT NULL,"
+               "    memo        TEXT,"
+               "    PRIMARY KEY(code,date)"
+               ");"),
+             
+             (@"CREATE INDEX idx_item_code ON SHItem (code);"),
+             (@"CREATE INDEX idx_item_date ON SHItem (date);")
+     ];
 }
 
 @end
