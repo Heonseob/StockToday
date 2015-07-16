fe734f3fbb6e2817f3f0f7bf922aabf2a78d8321
chase.stone <chase.stone@daumkakao.com>
2015-07-16 11:17:35 +0900

    extract code & name

--- StockToday/STMainViewController.m
+++ StockToday/STMainViewController.m
@@ -47,12 +47,40 @@
     
     [self.operationManager GET:url parameters:nil
                   success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                      NSString *data = [[NSString alloc] initWithUTF8String:[(NSData *)responseObject bytes]];
 
-                      NSDictionary* info = [data objectFromJSONString];
+                      NSString *stockInfo = [[NSString alloc] initWithUTF8String:[(NSData *)responseObject bytes]];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"timeinfo" withString:@"\"info\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"kospi" withString:@"\"kospi\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"kosdaq" withString:@"\"kosdaq\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"item" withString:@"\"item\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"date" withString:@"\"date\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"time" withString:@"\"time\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"message" withString:@"\"message\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"cost" withString:@"\"cost\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"updn" withString:@"\"updn\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"rate" withString:@"\"rate\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"code" withString:@"\"code\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"name" withString:@"\"name\""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"var dataset =" withString:@""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@"&nbsp;" withString:@""];
+                      stockInfo = [stockInfo stringByReplacingOccurrencesOfString:@";" withString:@""];
+                      
+                      NSDictionary* info = [stockInfo objectFromJSONString];
+                      NSArray *itemArray = [info objectForKey:@"item"];
+                      
+                      NSString *itemCode = nil, *itemName = nil;
+                      int index = 0;
+                      for (NSDictionary* item in itemArray)
+                      {
+                          itemCode = [item objectForKey:@"code"];
+                          itemName = [item objectForKey:@"name"];
+                        
+                          NSLog(@"%4d : [%@] %@", index++, itemCode, itemName);
+                      }
                       
                       
-                      NSLog(@"SUCCESS - %@", data);
+                      
+                      //NSLog(@"SUCCESS - %@", stockInfo);
                   } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
                       NSLog(@"FAIL - %@", error);
                   }];
